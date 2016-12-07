8eb0682563d2a6f4aaeaa524bbc4f3cd597835bf
Jaha96 <j_jaha28@yahoo.com>
2016-12-07 22:15:42 +0800

    Аялал бүртгэх, Хөтөч бүртгэх

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -8,8 +8,32 @@ namespace TravelWeb.Controllers
 {
     public class AdminController : Controller
     {
+        public static List<SelectListItem> items = new List<SelectListItem>();
+        public static SelectListItem[] getBloodType()
+        {
+            items.Clear();
+            items.Add(new SelectListItem { Text = "O+", Value = "O+" });
+            items.Add(new SelectListItem { Text = "O–", Value = "O–" });
+            items.Add(new SelectListItem { Text = "A+", Value = "A+" });
+            items.Add(new SelectListItem { Text = "A-", Value = "A-" });
+            items.Add(new SelectListItem { Text = "B+", Value = "B+" });
+            items.Add(new SelectListItem { Text = "B-", Value = "B-" });
+            items.Add(new SelectListItem { Text = "AB+", Value = "AB+" });
+            items.Add(new SelectListItem { Text = "AB-", Value = "AB-" });
+            return items.ToArray();
+        }
         // GET: Admin
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AylalRegister()
+        {
+            ViewBag.HutuchList = getBloodType();
+            return View();
+        }
+
+        public ActionResult HutuchRegister()
         {
             return View();
         }
---
+++ Models/Aylal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelWeb.Models
+{
+    public class Aylal
+    {
+        public int hutulburId { get; set; }
+        public string name { get; set; }
+        public string chiglel { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public string taniltsuulga { get; set; }
+        public string zurag { get; set; }
+        public int hutuchId { get; set; }
+    }
+}
